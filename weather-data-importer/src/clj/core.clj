964297066f0e39e5c4303ba85818f249c12c008c
Jacob Doran <j.doran.admin@gmail.com>
2023-12-19 15:37:13 +0800

    add xml parser dependency

--- weather-data-importer/src/clj/core.clj
+++ weather-data-importer/src/clj/core.clj
@@ -1,5 +1,6 @@
 (ns core
   (:require [honey.sql :as sql]
             [clojure.java.io :as io]
-            [next.jdbc :as jdbc]))
+            [next.jdbc :as jdbc]
+            [clojure.data.xml :as xml]))
 
