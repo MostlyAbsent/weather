c8ff461f42a9b8c9cd5572f2609718b17485a644
Jacob Doran <j.doran.admin@gmail.com>
2023-12-19 18:01:46 +0800

    initial parsers

--- weather-data-importer/src/clj/core.clj
+++ weather-data-importer/src/clj/core.clj
@@ -7,6 +7,36 @@
 ;; command line args parsing
 
 
+(defn- expiry-parser
+  [parsed-xml]
+  (->> parsed-xml
+       first
+       :content
+       (reduce (fn [acc x]
+                 (if (= (:tag x) :expiry-time)
+                   (first (:content x))
+                   acc)) "")))
+
+(defmulti area (fn [x] (get-in x [:attrs :type])))
+
+(defmethod area "region"
+  [{:keys [attrs]}]
+  {:region {:aac (:aac attrs)
+            :description (:description attrs)}})
+
+(defmethod area :default
+  [args]
+  )
+
+
+(defn- forecast-parser
+  [parsed-xml]
+  (->> parsed-xml
+       second
+       :content
+       (map area)
+       (filter #(not (nil? %)))
+       #_(map #(get-in % [:attrs :type] "notfound"))))
 
 ;; Transaction
 
