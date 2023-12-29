0101e618312d7748e2f5ffb1f7d05a6d5dce2c80
Jacob Doran <j.doran.admin@gmail.com>
2023-12-29 19:56:06 +0800

    feat: flattened data representation

--- weather-data-importer/src/clj/core.clj
+++ weather-data-importer/src/clj/core.clj
@@ -52,43 +52,66 @@
   {:unhandled-key true})
 
 (defn forecast-period-parser
-  [{:keys [attrs content]}]
+  [{:keys [attrs content]} aac parent description districts regions]
+  (let [district (->> districts
+                      (filter #(= parent (get %1 :aac)))
+                      first)
+        parentDescription (:description district)
+        regionKey (:parent district)
+        regionDescription (->> regions
+                               (filter #(= regionKey (get %1 :aac)))
+                               first
+                               :description)]
+    (merge {:index (:index attrs)
+            :startTime (unixtime (:start-time-utc attrs))
+            :endTime (unixtime (:end-time-utc attrs))
+            :locationKey aac
+            :locationDescription description
+            :districtDescription parentDescription
+            :districtKey parent
+            :regionKey regionKey
+            :regionDescription regionDescription}
+           (reduce #(conj %1  (forecast-content %2)) {} content))))
 
-  (merge {:index (:index attrs)
-          :startTime (unixtime (:start-time-utc attrs))
-          :endTime (unixtime (:end-time-utc attrs))}
-         (reduce #(conj %1  (forecast-content %2)) {} content)))
-
-(defmulti area (fn [x] (get-in x [:attrs :type])))
+(defmulti area (fn [x _] (get-in x [:attrs :type])))
 
 (defmethod area "region"
-  [{:keys [attrs]}]
+  [{:keys [attrs]} _]
   {:region {:aac (:aac attrs)
             :description (:description attrs)}})
 
 (defmethod area "public-district"
-  [{:keys [attrs]}]
+  [{:keys [attrs]} _]
   {:district {:aac (:aac attrs)
               :parent (:parent-aac attrs)
               :description (:description attrs)}})
 
 (defmethod area "location"
-  [{:keys [attrs content]}]
-  {:location {:aac (:aac attrs)
-              :parent (:parent-aac attrs)
-              :description (:description attrs)
-              :content (reduce #(conj %1 (forecast-period-parser %2)) [] content)}})
+  [{:keys [attrs content]} acc]
+  (let [aac (:aac attrs)
+        parent (:parent-aac attrs)
+        description (:description attrs)
+        districts (:districts acc)
+        regions (:regions acc)]
+    {:location (reduce #(conj %1 (forecast-period-parser %2 aac parent description districts regions)) [] content)}))
 
-(defmethod area :default
-  [_])
+(defn spread-areas
+  [acc x]
+  (let [a (area x acc)
+        k (first (keys a))
+        v (first (vals a))]
+    (cond
+      (= :region k) (merge acc {:regions (conj (:regions acc) v)})
+      (= :district k) (merge acc {:districts (conj (:districts acc) v)})
+      (= :location k) (merge acc {:locations (flatten (conj (:locations acc) v))}))))
 
 (defn- forecast-parser
   [parsed-xml]
   (->> parsed-xml
        second
        :content
-       (reduce #(conj %1 (area %2)) [])
-       (filter #(not (nil? %)))))
+       (reduce spread-areas {:districts [] :regions [] :locations []})
+       :locations))
 
 ;; Transaction
 
