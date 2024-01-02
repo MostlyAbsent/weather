a33885772db0f141dd62d762f33401d20877edc8
Jacob Doran <j.doran.admin@gmail.com>
2024-01-02 19:10:44 +0800

    refactor: drop honey.sql for next.jdbc.sql
    next.jdbc.sql has helper functions that handle batch processing nicely

--- weather-data-importer/src/clj/core.clj
+++ weather-data-importer/src/clj/core.clj
@@ -1,14 +1,14 @@
 (ns core
-  (:require [honey.sql :as sql]
-            [clojure.java.io :as io]
-            [next.jdbc :as jdbc]
-            [clojure.pprint :as pp]
-            [clojure.data.xml :as xml]
-            parsers)
+  (:require
+   [clojure.java.io :as io]
+   [next.jdbc :as jdbc]
+   [next.jdbc.sql :as sql]
+   [clojure.pprint :as pp]
+   [clojure.data.xml :as xml]
+   parsers)
   (:gen-class))
 
 ;; command line args parsing
-
 
 ;; Transaction
 
@@ -17,3 +17,12 @@
 ;; Insert
 
 ;; invoke main
+
+(defn new-forecast-id!
+  "Inserts the expiry as a new forecast and returns the ID generated."
+  [db-conn expiry]
+  (let [forecast-id (-> (sql/insert! db-conn
+                                       :forecast {:expiry expiry} {:suffix "RETURNING *"})
+                        :Forecast/id)]
+    {:id forecast-id}))
+
